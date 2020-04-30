61660f792da2fc4a4aef9f57993121e390ff569f
byx <byx@DESKTOP-SCFT0GJ>
2020-04-30 17:32:53 +0800

    改进FullScreenCommand

---
+++ LeYun/ViewModel/WindowBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace LeYun.ViewModel
+{
+    class WindowBehavior
+    {
+        // 窗口加载事件
+        public static ICommand GetLoadedCommand(DependencyObject obj)
+        {
+            return (ICommand)obj.GetValue(LoadedCommandProperty);
+        }
+        public static void SetLoadedCommand(DependencyObject obj, ICommand value)
+        {
+            obj.SetValue(LoadedCommandProperty, value);
+        }
+        public static readonly DependencyProperty LoadedCommandProperty =
+            DependencyProperty.RegisterAttached("LoadedCommand", typeof(ICommand), typeof(WindowBehavior), new PropertyMetadata(new PropertyChangedCallback(OnLoadedCommandChanged)));
+
+        private static void OnLoadedCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            Window window = (Window)d;
+            window.Loaded += Window_Loaded;
+        }
+
+        private static void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Window window = (Window)sender;
+            ICommand command = GetLoadedCommand(window);
+            command.Execute(e);
+        }
+    }
+}
---
+++ WPFCustomControls/AnimationWindow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFCustomControls
+{
+    public class AnimationWindow : Window
+    {
+        static AnimationWindow()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AnimationWindow), new FrameworkPropertyMetadata(typeof(AnimationWindow)));
+        }
+
+        // 构造函数
+        public AnimationWindow()
+        {
+            Closing += AnimationWindow_Closing;
+        }
+
+        private void AnimationWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //MessageBox.Show("123");
+        }
+    }
+}
